37e502db423ac84420fc0813047d0266355e8e71
LarsStorholt <70659086+LarsStorholt@users.noreply.github.com>
2021-02-18 13:51:56 +0100

    java11

--- src/main/java/oslomet/spring/heiController.java
+++ src/main/java/oslomet/spring/heiController.java
@@ -1,13 +1,14 @@
 package oslomet.spring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /*public Arraylist<Kunde>kunderegister*/
 
 @RestController
 public class heiController {
-    @GetMapping
+    @PostMapping("/")
     public Kunde enKunde(Kunde innKunde){
         return innKunde;
     }
