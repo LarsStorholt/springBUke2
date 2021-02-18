07e92de0d5e872e48d5a4c1d9652d17f95756b2f
LarsStorholt <70659086+LarsStorholt@users.noreply.github.com>
2021-02-18 13:48:49 +0100

    Initial commit

---
+++ src/main/java/oslomet/spring/heiController.java
@@ -0,0 +1,14 @@
+package oslomet.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public Arraylist<Kunde>kunderegister
+
+@RestController
+public class heiController {
+    @GetMapping
+    public Kunde enKunde(Kunde innKunde){
+        return innKunde;
+    }
+}
